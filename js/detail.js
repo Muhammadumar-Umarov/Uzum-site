ce6dffde6b426808bddd31f01e5e29f1ede80562
Muhammadumar-Umarov <surnameen22@gmail.com>
2025-05-04 17:54:24 +0500

    updated

--- js/detail.js
+++ js/detail.js
@@ -1,21 +1,33 @@
 import { fetchData } from "./main.js"
 const boxEl = document.querySelector(".box")
 const params = new URLSearchParams(location.search)
+const collectionEl = document.querySelector(".collection")
 
-function renderRecipeDetail (data){
-  console.log(data);
+function renderRecipeDetail(data) {
   boxEl.innerHTML = `
-     <div>
-        <img src=${data.images} width="400" alt=${data.title}>
-      </div>
-      <div>
-        <h1>${data.title}</h1>
-        <p>${data.price}$</p>
-      </div>
+     
   `
 }
 
-window.onload = ()=>{
-  const id = params.get("id")
-  fetchData(`/products/${id}`,renderRecipeDetail, ()=>{} )
+function renderCategoryList(data) {
+  data.forEach((tag) => {
+    const li = document.createElement("li")
+    li.innerHTML = tag
+    collectionEl.appendChild(li)
+  })
 }
+
+window.onload = () => {
+  const id = params.get("id")
+  fetchData(`/products/${id}`, renderRecipeDetail, () => { })
+  fetchData("/products/category-list", renderCategoryList, () => { })
+}
+
+
+//       <div>
+//         <img src=${data.images} width="400" alt=${data.title}>
+//       </div>
+//       <div>
+//         <h1>${data.title}</h1>
+//         <p>${data.price}$</p>
+//       </div>
